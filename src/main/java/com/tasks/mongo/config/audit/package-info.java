e5cb3652f5ccec765b38325a1679a87324846116
Oleg Bezkorovaynyi <Oleg.Bezkorovaynyi@Sophos.com>
2018-03-05 18:42:17 +0200

    Initial application generated by JHipster-4.14.1

---
+++ src/main/java/com/tasks/mongo/config/audit/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Audit specific code.
+ */
+package com.tasks.mongo.config.audit;
---
+++ src/main/java/com/tasks/mongo/repository/AuthorityRepository.java
@@ -0,0 +1,11 @@
+package com.tasks.mongo.repository;
+
+import com.tasks.mongo.domain.Authority;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+/**
+ * Spring Data MongoDB repository for the Authority entity.
+ */
+public interface AuthorityRepository extends MongoRepository<Authority, String> {
+}
